b40dbc6357f334c3d2a90235f4a93e94e9092862
Roman Veskera <roman.veskera@plvision.eu>
2025-12-08 16:10:05 +0200

    fix: Fixed whole program
    The changes were made in accordance with the change in the task conditions, namely parsing the input file, one thread parses and writes to the result.txt file, the other thread reads and sorts the received lines.
    Now program works with two threads, which are using conditional variable and mutex.
    
    Signed-off-by: Roman Veskera <roman.veskera@plvision.eu>

--- main.c
+++ main.c
@@ -1,104 +1,148 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define NUM_ROUNDS 1000
-#define ARRAY_SIZE 10
+#define LINE_SIZE 128
+
+pthread_mutex_t lock;
+pthread_cond_t buffer_empty;
+pthread_cond_t data_ready;
+
+int buffer_filled = 0; //flag 0 - empty, 1 - ready
+int EOF_reached = 0;
 
 typedef struct {
-	int* arr;
-	unsigned int seed;
+	char buffer[LINE_SIZE + 1];
+	char sorted_buffer[LINE_SIZE + 1];
+	FILE* input_fp;
 	int id;
 } ThreadArgs;
 
 void* fill_arr(void* arg) {
 	ThreadArgs* args = (ThreadArgs*)arg;
+	int current_line = 0;
 
-	FILE* fptr = fopen("arr_file.txt", "a");
-	if (fptr == NULL) {
-		printf("Error opening file");
+	FILE* input_fp = fopen("input_text.txt", "r");
+	if (!input_fp) {
+		printf("Error opening input_text.txt");
 		pthread_exit(NULL);
 	}
 
-	fprintf(fptr, "Generated [%d] - ", args->id);
 
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		args->arr[i] = rand_r(&args->seed) % 101;
-		if (i == ARRAY_SIZE - 1) {
-			fprintf(fptr, "%d", args->arr[i]);
-		} else {
-			fprintf(fptr, "%d,", args->arr[i]);
+	while (1) {
+		pthread_mutex_lock(&lock);
+
+		while (buffer_filled == 1 && !EOF_reached) {
+			pthread_cond_wait(&buffer_empty, &lock);
 		}
-	}
-	fprintf(fptr, "\n");
 
-	fclose(fptr);
+		if (EOF_reached) {
+			pthread_mutex_unlock(&lock);
+			break;
+		}
+
+		if (fgets(args->buffer, LINE_SIZE + 1, input_fp) == NULL) {
+			EOF_reached = 1;
+			buffer_filled = 0;
+			pthread_mutex_unlock(&lock);
+			pthread_cond_signal(&data_ready);
+			break;
+		}
+
+		size_t len = strlen(args->buffer);
+		if (len > 0 && args->buffer[len - 1] == '\n') {
+			args->buffer[len - 1] = '\0';
+		}
+
+		strcpy(args->sorted_buffer, args->buffer);
+
+		args->id = current_line++;
+
+		FILE* output = fopen("result.txt", "a");
+		if (output) {
+			fprintf(output, "T[%d] - %s\n", args->id, args->buffer);
+			fclose(output);
+		}
+
+		buffer_filled = 1;
+
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&data_ready);
+	}
+
+	fclose(input_fp);
 	pthread_exit(NULL);
 }
 
 void* sort_arr(void* arg) {
 	ThreadArgs* args = (ThreadArgs*)arg;
 
-	for (int i = 0; i < ARRAY_SIZE - 1; i++) {
-		for (int j = 0; j < ARRAY_SIZE - i - 1; j++) {
-			if (args->arr[j] > args->arr[j + 1]) {
-				int temp = args->arr[j];
-				args->arr[j] = args->arr[j + 1];
-				args->arr[j + 1] = temp;
+	while (1) {
+		pthread_mutex_lock(&lock);
+
+		while (buffer_filled == 0 && !EOF_reached) {
+			pthread_cond_wait(&data_ready, &lock);
+		}
+
+		if (EOF_reached && buffer_filled == 0) {
+			pthread_mutex_unlock(&lock);
+			break;
+		}
+
+		size_t len = strlen(args->sorted_buffer);
+
+		for (size_t i = 0; i + 1 < len; i++) {
+			for (size_t j = 0; j + 1 < len - i; j++) {
+				if (args->sorted_buffer[j] > args->sorted_buffer[j + 1]) {
+					char temp = args->sorted_buffer[j];
+					args->sorted_buffer[j] = args->sorted_buffer[j + 1];
+					args->sorted_buffer[j + 1] = temp;
+				}
 			}
 		}
-	}
 
-	FILE* file = fopen("arr_file.txt", "a");
-	if (file == NULL) {
-		printf("Error opening file");
-		pthread_exit(NULL);
-	}
-
-	fprintf(file, "Sorted [%d] - ", args->id);
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		if (i == ARRAY_SIZE - 1) {
-			fprintf(file, "%d", args->arr[i]);
-		} else {
-			fprintf(file, "%d,", args->arr[i]);
+		FILE* output = fopen("result.txt", "a");
+		if (output) {
+			fprintf(output, "Sorted T[%d] - %s\n", args->id, args->sorted_buffer);
+			fprintf(output, "----------------\n");
+			fclose(output);
 		}
-	}
-	fprintf(file, "\n----------------\n");
 
-	fclose(file);
+		buffer_filled = 0;
+
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&buffer_empty);
+	}
+
 	pthread_exit(NULL);
 }
 
 int main() {
-	int shared_arr[ARRAY_SIZE];
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&buffer_empty, NULL);
+	pthread_cond_init(&data_ready, NULL);
+	char buffer[LINE_SIZE + 1];
 	pthread_t thread_fill, thread_sort;
 
-	FILE* clean = fopen("arr_file.txt", "w");
+	FILE* clean = fopen("result.txt", "w");
 	if (clean) fclose(clean);
 
 	ThreadArgs args;
-	args.arr = shared_arr;
-	args.seed = time(NULL);
 
-	for (int i = 0; i < NUM_ROUNDS; i++) {
-		args.id = i;
+	pthread_create(&thread_sort, NULL, sort_arr, (void*)&args);
 
-		if(pthread_create(&thread_fill, NULL, fill_arr, (void*)&args) != 0) {
-			printf("Failed to create fill thread");
-			return 1;
-		}
+	pthread_create(&thread_fill, NULL, fill_arr, (void*)&args);
 
-		pthread_join(thread_fill, NULL);
+	pthread_join(thread_sort, NULL);
+	pthread_join(thread_fill, NULL);
 
-		if (pthread_create(&thread_sort, NULL, sort_arr, (void*)&args) != 0) {
-			printf("Failed to create sort thread");
-			return 1;
-		}
+	pthread_mutex_destroy(&lock);
+	pthread_cond_destroy(&data_ready);
+	pthread_cond_destroy(&buffer_empty);
 
-		pthread_join(thread_sort, NULL);
-	}
-
-	printf("Done! Check arr_file.txt\n");
+	printf("Done! Check result.txt\n");
 	return 0;
 }
